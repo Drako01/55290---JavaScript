0c838c2ba43dd08b220eff42e78244a26774fa1a
Alejandro Di Stefano <addistefano@live.com.ar>
2023-06-27 21:11:08 -0300

    Clase 01 con Script por fuera del HTML

---
+++ index.js
@@ -0,0 +1,35 @@
+let letra = "c" // Le asignamos con un string que va con comillas
+let letras = "d"
+
+letra = "e"
+letra = "f"
+let g = "Hola"
+letra = 5 // 5 es un Number y no lleva comillas - Eso lo diferencia de un String como por ejemplo "5"
+
+const numerito = '0'
+
+const numerito5 = 0
+
+
+
+//console.log(letra)
+//console.log(numerito)
+
+
+let nombreUsuario = prompt("Ingrese su nombre")
+
+console.log(nombreUsuario)
+
+
+let nombreUsuario1 = "Su nombre es Pirulo"
+
+console.log(nombreUsuario1)
+
+alert("Hola Gente " + nombreUsuario)
+
+// let nombreUsuario = prompt("Ingrese su nombre")
+// alert("Su nombre es: " + nombreUsuario)
+
+// let usuario = prompt("Ingrese su nombre y apellido")
+
+// alert("hola! bienvenido " + usuario)
