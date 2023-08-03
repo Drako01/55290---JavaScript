752f3b8797a49c26cf5b3a36076251e5eb59a84d
Alejandro Di Stefano <addistefano@live.com.ar>
2023-08-03 14:58:48 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const d = document // Guardo el objeto document en una constante
+const cuerpo = d.body // Guardo el objeto body en una constante
+cuerpo.style.backgroundColor = '#222' // Cambio el color de fondo del body
+const cabeza = document.head
+const titulo = document.createElement('title')
+const cabecera = document.getElementById('header')
+const banner = titulo.innerText = 'Comisión #55290'
+cabeza.appendChild(titulo)
+const fotter = document.querySelector('#p-footer');
+const anio = new Date().getFullYear();
+fotter.innerHTML = `<a href="https://www.coderhouse.com/" target="_blank">&copy; CoderHouse | JavaScript | ${banner} | ${anio}</a>`
+
+// Operadores y condicionales avanzados
