92093e5819f28249fd078d9e34f679f614617383
Alejandro Di Stefano <addistefano@live.com.ar>
2023-08-10 20:31:02 -0300

    Clase 14

--- js/index.js
+++ js/index.js
@@ -11,11 +11,48 @@ const anio = new Date().getFullYear();
 fotter.innerHTML = `<a href="https://www.coderhouse.com/" target="_blank">&copy; CoderHouse | JavaScript | ${banner} | ${anio}</a>`
 
 // Librerias
-
-const botonAlerta = d.getElementById('botonAlerta')
-
-botonAlerta.addEventListener('click', () => {
-    alert('Hice Click.!!');
-});
-
 // SweetAlert
+const botonAlerta = d.getElementById('botonAlerta')
+const btn = d.getElementById('btn')
+const inputA = d.createElement('input')
+inputA.type = 'number'
+inputA.id = 'edad'
+
+botonAlerta.prepend(inputA)
+
+// btn.addEventListener('click', () => {
+//     if(inputA.value >= 18) {
+//         Swal.fire('Puede Ingresar.!', 'Cumple con los requisitos de Edad', 'success')
+//     } else {
+//         Swal.fire('No Puede Ingresar.!', 'Es menor de Edad', 'error')
+//     }
+// });
+
+
+// Toastify
+
+// btn.addEventListener('click', () => {
+//     Toastify({
+//         text: "This is a toast",
+//         duration: 3000,
+//         destination: "https://google.com",
+//         newWindow: true,
+//         close: true,
+//         gravity: "top", // `top` or `bottom`
+//         position: "left", // `left`, `center` or `right`
+//         stopOnFocus: true, // Prevents dismissing of toast on hover
+//         style: {
+//             background: "linear-gradient(to right, #00b09b, #96c93d)",
+//         },
+//         onClick: function () { } // Callback after click
+//     }).showToast();
+// });
+
+// Luxon
+
+// Pendiente
+
+var DateTime = luxon.DateTime
+let fecha = DateTime.now().setZone('America/Buenos_Aires').minus({weeks:1}).endOf('day').toISO();
+console.log(fecha)
+
