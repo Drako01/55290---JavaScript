d5e3cb8a599f0c8e37ee2676e76c14ecc53fa7ea
Alejandro Di Stefano <addistefano@live.com.ar>
2023-08-18 16:03:18 -0300

    Initial commit

--- js/index.js
+++ js/index.js
@@ -10,26 +10,5 @@ const fotter = d.querySelector('#p-footer');
 const anio = new Date().getFullYear();
 fotter.innerHTML = `<a href="https://www.coderhouse.com/" target="_blank">&copy CoderHouse | JavaScript | ${banner} | ${anio}</a>`
 
-// Creacion de Elementos para Clase de Ajax
-const formFind = d.getElementById('busqueda')
-const input = d.createElement('input')
-const btn = d.createElement('button')
-input.classList = 'form-control'
-input.type = 'number'
-input.placeholder = 'Ingrese un Número'
-input.min = 1
-formFind.classList = 'container coderhouse div-busqueda'
-btn.classList = 'btn btn-primary busqueda'
-btn.innerText = 'Buscar'
-btn.type = 'submit'
-formFind.append(input)
-formFind.append(btn)
-const lista = d.querySelector('#listado');
-// Desde el Formulario:
-const formUser = d.getElementById('form-users')
-const inputName = d.getElementById('name-imput')
-const inputLastName = d.getElementById('last-name-imput')
-
-
 /// Async Await 
 // Ejemplo Usando la API de Mercado Libre
