3a19dd34d5e929edeca63d98a763fdd5d417cf7f
Alejandro Di Stefano <addistefano@live.com.ar>
2023-08-18 15:18:51 -0300

    Usando la API de MercadoLibre

--- js/index.js
+++ js/index.js
@@ -227,12 +227,36 @@ const inputLastName = d.getElementById('last-name-imput')
 // .catch((error) => (console.error(error)))
 
 
-/// Async Await
-
-const peticion = async () => {
-    const respuesta = await fetch('https://64dd48f6e64a8525a0f7cb2c.mockapi.io/estudiantes')
+/// Async Await 
+// Ejemplo Usando la API de Mercado Libre
+const peticion1 = async () => {
+    const respuesta = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=Samsung')
     const datos = await respuesta.json()
-    console.log(datos)
+    const data = await datos.results
+    console.log(data)
+}
+peticion1()
+
+// Ejemplo Usando la API de Mercado Libre en el DOM
+const cards = d.getElementById('cards')
+const peticion2 = async () => {
+    const respuesta = await fetch('https://api.mercadolibre.com/sites/MLA/search?q=Samsung')
+    const datos = await respuesta.json()
+    const data = await datos.results
+    for (item of data) {
+        const card = d.createElement('div')
+        card.innerHTML = `
+        <div class="card" style="width: 18rem; height: 32rem;">
+            <img class="card-img-top" src=${item.thumbnail} alt=${item.title}/>
+            <div class="card-body">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">Precio: $${item.price}.-</p>
+                <a href="#" class="btn btn-primary">Comprar</a>
+            </div>     
+        </div>       
+        `
+        cards.append(card)
+    }
 }
 
-peticion()
+peticion2()
